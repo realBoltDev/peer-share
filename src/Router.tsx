7427f7668fa0958bc69ae0160d4d2e0da1826f71
beastxbolt <harsit2405@gmail.com>
2025-11-08 15:15:31 +0530

    feat: Link Connect & About pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { BaseLayout } from '@/layouts/BaseLayout'
 import { HomePage } from '@/pages/Home/Home.page';
+import { ConnectPage } from '@/pages/Connect/Connect.page';
+import { AboutPage } from '@/pages/About/About.page';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <BaseLayout />,
     children: [
-      { index: true, element: <HomePage />}
+      { index: true, element: <HomePage />},
+      { path: 'connect', element: <ConnectPage /> },
+      { path: 'about', element: <AboutPage /> }
     ]
   },
 ]);
