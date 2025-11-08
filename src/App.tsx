6f9d9cffb92d19c6203e2a10fb35decde753c31b
beastxbolt <harsit2405@gmail.com>
2025-11-08 15:10:28 +0530

    chore: Update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import BaseLayout from '@/layouts/BaseLayout'
-import Home from '@/pages/Home'
-import Share from '@/pages/Share'
-import Receive from '@/pages/Connect'
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+import { Router } from './Router';
+import { theme } from './theme';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<BaseLayout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/share' element={<Share />} />
-          <Route path='/connect' element={<Share />} />
-          <Route path='/about' element={<Share />} />
-          <Route path='/receive/:sessionId' element={<Receive />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    <MantineProvider theme={theme} forceColorScheme='dark'>
+      <Router />
+    </MantineProvider>
+  );
 }
