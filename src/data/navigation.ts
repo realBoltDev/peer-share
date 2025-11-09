6e48d15ebb50e63bcddf2af29ce3a7c3202ac439
beastxbolt <harsit2405@gmail.com>
2025-11-09 16:20:34 +0530

    feat: Add navigation data

---
+++ src/data/navigation.ts
@@ -0,0 +1,12 @@
+import type { NavigationSection } from "@/types";
+import {
+  IconHome,
+  IconInfoCircle,
+  IconPlugConnected,
+} from '@tabler/icons-react';
+
+export const navigationData: NavigationSection[] = [
+  { link: '/', label: 'Home', icon: IconHome },
+  { link: '/connect', label: 'Connect', icon: IconPlugConnected },
+  { link: '/about', label: 'About', icon: IconInfoCircle }
+];
