6417f751aaf2230f3c108074b455846473483d50
beastxbolt <harsit2405@gmail.com>
2025-11-10 16:42:02 +0530

    feat: Add new components

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,56 +1,32 @@
 import { Paper, Text, useMantineTheme } from "@mantine/core";
-import { ConnectionStatus } from "@/components/ConnectionStatus/ConnectionStatus";
+import { StatusPanel } from "@/components/StatusPanel/StatusPanel";
+import { FileUpload } from "@/components/FileUpload/FileUpload";
 import classes from './Home.module.css';
+import { useState } from "react";
 
 export function HomePage() {
   const theme = useMantineTheme();
 
+  const [nickname, setNickname] = useState("BOLT");
+  const [peerId, setPeerId] = useState('ABC123');
+  const [status, setStatus] = useState('waiting');
+  const [dropDisabled, setDropDisabled] = useState(false);
+
   return (
-    <div style={{
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-      gap: 0
-    }}>
-      <Paper
-        shadow="xs"
-        radius="sm"
-        p="md"
-        bg={theme.colors.dark[6]}
-        mt="md"
-        ml="md"
-        mr="md"
-      >
-        <Text size="25px" c="white" ta="center">Fast, Private, Peer-to-Peer file sharing.</Text>
+    <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', gap: 0 }}>
+      <Paper shadow="xs" radius="sm" p="md" bg={theme.colors.dark[6]} mt="md" ml="md" mr="md">
+        <Text size="25px" c="white" ta="center">
+          Fast, Private, Peer-to-Peer file sharing.
+        </Text>
       </Paper>
 
-      <Paper
-        shadow="xs"
-        radius="sm"
-        p="md"
-        bg={theme.colors.dark[6]}
-        m="md"
-        style={{ flex: 1, display: 'flex', flexDirection: 'column', gap: 'md' }}
-      >
-        <Paper
-          shadow="xs"
-          radius="sm"
-          p="md"
-          mb="md"
-          bg={theme.colors.dark[5]}
-        >
-          <ConnectionStatus status="waiting" peerId="ABC123" nickname="BOLT"></ConnectionStatus>
-        </Paper>
+      <Paper shadow="xs" radius="sm" p="md" bg={theme.colors.dark[6]} m="md" style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+        <StatusPanel status={status} peerId={peerId} nickname={nickname} setNickname={setNickname} />
 
-        <Paper
-          shadow="xs"
-          radius="sm"
-          p="md"
-          bg={theme.colors.dark[5]}
-          style={{ flex: 1 }}
-        >
-          <Text c="white">File dropdown will go here</Text>
+        <FileUpload dropDisabled={dropDisabled} />
+
+        <Paper shadow="xs" radius="sm" p="md" bg={theme.colors.dark[5]} mt="md" style={{ flex: 1 }}>
+          <Text c="white">Files metadata will be shown here</Text>
         </Paper>
       </Paper>
     </div>
