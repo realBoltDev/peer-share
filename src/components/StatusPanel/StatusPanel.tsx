aced0e652ea707f1aea9ad29ee6603f5a91713b2
beastxbolt <harsit2405@gmail.com>
2025-11-12 20:54:48 +0530

    chore: Move function parameter types

--- src/components/StatusPanel/StatusPanel.tsx
+++ src/components/StatusPanel/StatusPanel.tsx
@@ -1,8 +1,9 @@
 import { Grid } from '@mantine/core';
 import { ClientInfo } from '@/components/ClientInfo/ClientInfo';
 import { ConnectionStatus } from '@/components/ConnectionStatus/ConnectionStatus';
+import { StatusPanelProps } from '@/types';
 
-export function StatusPanel({ status, peerId, nickname, setNickname }: { status: string, peerId: string, nickname: string, setNickname: (value: string) => void }) {
+export function StatusPanel({ status, peerId, nickname, setNickname }: StatusPanelProps) {
   return (
     <Grid gutter="md">
       <Grid.Col span={{ base: 12, sm: 6 }}>
