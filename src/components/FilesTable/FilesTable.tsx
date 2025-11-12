1e7fde4c02a736b9c654dfeaf8869d0ad342af49
beastxbolt <harsit2405@gmail.com>
2025-11-12 20:52:20 +0530

    chore: Update files table

--- src/components/FilesTable/FilesTable.tsx
+++ src/components/FilesTable/FilesTable.tsx
@@ -1,8 +1,11 @@
-import { Group, Progress, Table, Text, Tooltip, ActionIcon } from '@mantine/core';
-import { IconUpload } from '@tabler/icons-react';
+import { Paper, Group, Progress, Table, Text, Tooltip, ActionIcon, useMantineTheme } from '@mantine/core';
+import { IconUpload, IconCheck, IconX, IconLoader2 } from '@tabler/icons-react';
 import { FileProps } from '@/types';
+import classes from './FilesTable.module.css';
 
 export function FilesTable({ data }: { data: FileProps }) {
+  const theme = useMantineTheme();
+
   const rows = data.map((row) => {
     return (
       <Table.Tr key={row.filename}>
@@ -33,37 +36,54 @@ export function FilesTable({ data }: { data: FileProps }) {
               {row.progress}%
             </Text>
           </Group>
-          <Progress value={row.progress} animated />
+          <Progress value={row.progress} animated={row.status === 'Uploading' ? true : false} />
         </Table.Td>
 
         <Table.Td>
           {row.status}
         </Table.Td>
         <Table.Td>
-          <ActionIcon>
-            <IconUpload size={15} stroke={1.8} />
-          </ActionIcon>
+          {row.status === 'Pending' ?
+            <ActionIcon color='blue'>
+              <IconUpload size={15} stroke={1.8} />
+            </ActionIcon>
+            :
+            row.status === 'Uploading' ?
+              <ActionIcon color='blue' style={{ pointerEvents: 'none' }}>
+                <IconLoader2 className={classes.spin} size={15} stroke={1.8} />
+              </ActionIcon>
+              :
+              row.status === 'Completed' ?
+                <ActionIcon color='green' style={{ pointerEvents: 'none' }}>
+                  <IconCheck size={15} stroke={1.8} />
+                </ActionIcon>
+                :
+                <ActionIcon color='red' style={{ pointerEvents: 'none' }}>
+                  <IconX size={15} stroke={1.8} />
+                </ActionIcon>}
         </Table.Td>
       </Table.Tr>
     );
   });
 
   return (
-    <Table.ScrollContainer minWidth={800}>
-      <Table striped verticalSpacing="xs">
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>Filename</Table.Th>
-            <Table.Th>Size</Table.Th>
-            <Table.Th>Speed</Table.Th>
-            <Table.Th>Progress</Table.Th>
-            <Table.Th>Status</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {rows}
-        </Table.Tbody>
-      </Table>
-    </Table.ScrollContainer>
+    <Paper shadow="xs" radius="sm" p="md" bg={theme.colors.dark[5]} mt="md" style={{ flex: 1 }}>
+      <Table.ScrollContainer minWidth={800}>
+        <Table striped verticalSpacing="xs">
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th>Filename</Table.Th>
+              <Table.Th>Size</Table.Th>
+              <Table.Th>Speed</Table.Th>
+              <Table.Th>Progress</Table.Th>
+              <Table.Th>Status</Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>
+            {rows}
+          </Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
+    </Paper>
   );
 }
