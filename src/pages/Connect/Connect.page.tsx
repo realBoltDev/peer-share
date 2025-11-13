3f9d8be4a93be8410bebdba2bcd9112b16842502
beastxbolt <harsit2405@gmail.com>
2025-11-13 10:57:09 +0530

    chore: Remove top paper element

--- src/pages/Connect/Connect.page.tsx
+++ src/pages/Connect/Connect.page.tsx
@@ -14,12 +14,6 @@ export function ConnectPage() {
 
   return (
     <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', gap: 0 }}>
-      <Paper shadow='xs' radius='sm' p='md' bg={theme.colors.dark[6]} mt='md' ml='md' mr='md'>
-        <Text size='25px' c='white' ta='center'>
-          Receive files securely. Direct and private.
-        </Text>
-      </Paper>
-
       <Paper shadow='xs' radius='sm' p='md' bg={theme.colors.dark[6]} m='md' style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         <StatusPanel status={status} peerId={peerId} nickname={nickname} setNickname={setNickname} />
       </Paper>
