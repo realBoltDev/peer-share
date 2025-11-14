f83a16aef15bbdd9ab170e8e86f554841aef8b4d
beastxbolt <harsit2405@gmail.com>
2025-11-14 12:08:29 +0530

    feat: Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import http from 'http';
+import { Server } from 'socket.io';
+import { initSockets } from './sockets/index.js';
+import { app } from './app.js';
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+initSockets(io);
+
+app.get('/', (req, res) => {
+    res.status(200).json({ success: true, message: 'PeerShare Server active'});
+});
+
+server.listen(3000, () => {
+    console.log('[INFO] Server running on port 3000');
+})
