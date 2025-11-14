99c83aeb8c72c747b66f93ff252716e16267a5a2
beastxbolt <harsit2405@gmail.com>
2025-11-14 12:14:09 +0530

    feat: Add siganling handler

---
+++ backend/src/sockets/signaling.handler.ts
@@ -0,0 +1,48 @@
+import { Server, Socket } from 'socket.io';
+import { redisClient } from '../redis/client.js';
+import { generatePeerId, generateNickname } from '../utils/peer.js';
+
+export function registerSignalingHandlers(socket: Socket, io: Server): void {
+  socket.on('register', async () => {
+    let peerId = generatePeerId();
+
+    while (await redisClient.exists(`peer:${peerId}`)) {
+      peerId = generatePeerId();
+    }
+
+    const nickname = generateNickname();
+    await redisClient.hSet(`peer:${peerId}`, { connected: 'false', nickname: nickname, remotePeerId: '', remoteNickname: '', socketId: socket.id });
+
+    socket.data.peerId = peerId;
+    socket.data.nickname = nickname;
+
+    socket.emit('registered', { peerId: peerId, nickname: nickname });
+    console.log(`[INFO] Registered | Peer ID: ${peerId} | Nickname: ${nickname}`);
+  });
+
+  socket.on('update:nickname', async ({ nickname }) => {
+    const peerId = socket.data.peerId;
+    if (!peerId) return;
+
+    await redisClient.hSet(`peer:${peerId}`, { nickname });
+    socket.data.nickname = nickname;
+
+    const allKeys = await redisClient.keys('peer:*');
+    for (const key of allKeys) {
+      const peerData = await redisClient.hGetAll(key);
+      if (peerData.remotePeerId === peerId && peerData.socketId) {
+        io.to(peerData.socketId).emit('updateRemoteNickname', { nickname });
+        break;
+      }
+    }
+  });
+
+  socket.on('disconnect', async (reason) => {
+    console.log(`[INFO] Peer diconnected: ${socket.id}`);
+
+    const peerId = socket.data.peerId;
+    if (peerId) {
+      await redisClient.del(`peer:${peerId}`);
+    }
+  });
+}
