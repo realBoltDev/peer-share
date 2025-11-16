761ea8b76070c9b76ec869bc943b36bf699c3672
beastxbolt <harsit2405@gmail.com>
2025-11-16 13:27:23 +0530

    feat: Add socket init

--- backend/src/sockets/index.ts
+++ backend/src/sockets/index.ts
@@ -1,11 +1,15 @@
 import { Server } from 'socket.io';
 import { registerSignalingHandlers } from './signaling.handler.js';
+import { registerConnectionHandler } from './connection.handler.js';
+import { registerInitHandler } from './init.handler.js';
 
 export function initSockets(io: Server) {
   io.on('connection', (socket) => {
     console.log(`[INFO] Peer connected: ${socket.id}`);
 
+    registerInitHandler(socket);
     registerSignalingHandlers(socket, io);
+    registerConnectionHandler(socket, io);
   });
 
   io.on('connect_error', (error) => {
