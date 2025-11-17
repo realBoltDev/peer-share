4d2d11cadb9e5b9d772720bec1fd3ec1642ba192
beastxbolt <harsit2405@gmail.com>
2025-11-17 12:55:05 +0530

    feat: Add connectionSlice

---
+++ frontend/src/store/slices/connectionSlice.ts
@@ -0,0 +1,38 @@
+import { StateCreator } from 'zustand';
+
+export interface ConnectionSlice {
+  mode: string | null;
+  sendStatus: string | null;
+  sendMessage: string | null;
+  receiveStatus: string | null;
+  receiveMessage: string | null;
+  setMode: (mode: string | null) => void;
+  setSendStatus: (status: string | null, message: string | null) => void;
+  setReceiveStatus: (status: string | null, message: string | null) => void;
+}
+
+export const createConnectionSlice: StateCreator<
+  ConnectionSlice,
+  [],
+  [],
+  ConnectionSlice> = (set) => ({
+    mode: null,
+
+    sendStatus: null,
+    sendMessage: null,
+
+    receiveStatus: null,
+    receiveMessage: null,
+
+    setMode: (mode) => {
+      set({ mode })
+    },
+
+    setSendStatus: (status, message) => {
+      set({ sendStatus: status, sendMessage: message });
+    },
+
+    setReceiveStatus: (status, message) => {
+      set({ receiveStatus: status, receiveMessage: message });
+    }
+  });
