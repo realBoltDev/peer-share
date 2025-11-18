61611c89e99d89f3800cd95dd042e428fb76922b
beastxbolt <harsit2405@gmail.com>
2025-11-18 14:21:30 +0530

    chore: Update socket events

--- frontend/src/api/handlers/registerConnectionHandler.ts
+++ frontend/src/api/handlers/registerConnectionHandler.ts
@@ -3,42 +3,45 @@ import { appStore } from "@/store/appStore";
 
 export function registerConnectionHandler(socket: Socket) {
   const {
-    remotePeerId,
     setRemotePeerId,
     setRemoteNickname,
     setRemotePeerConnected,
     setSendStatus,
     setReceiveStatus,
+    setSendMessage,
+    setReceiveMessage,
     setConnectBtnLoading,
     setRequestModalOpened,
     setRequestModalPeer,
-    setRequestModalNickname
+    setRequestModalNickname,
+    initPeerConnection
   } = appStore.getState();
 
-  socket.on('connection:requestSent', ({ message }) => {
-    setReceiveStatus('waiting', message);
+  socket.on('connection:requestSent', (message) => {
+    setReceiveStatus('waiting');
+    setReceiveMessage(message);
   });
 
-  socket.on('connection:failed', ({ message, mode }) => {
-    if (mode === 'send') {
-      setSendStatus('failed', message);
-    } else {
-      setReceiveStatus('failed', message);
-      setConnectBtnLoading(false);
-    }
+  socket.on('connection:failed', (data) => {
+    console.error('[Connection] Connection failed:', data);
+    setReceiveStatus('failed');
+    setReceiveMessage(data.message);
+    setConnectBtnLoading(false);
   });
 
-  socket.on('connection:incoming', ({ from }) => {
-    setRequestModalPeer(from.peerId);
-    setRequestModalNickname(from.nickname);
+  socket.on('connection:incoming', (receiver) => {
+    setRequestModalPeer(receiver.peerId);
+    setRequestModalNickname(receiver.nickname);
     setRequestModalOpened(true);
   });
 
-  socket.on('connection:accepted', ({ from }) => {
-    setRemotePeerId(from.peerId);
-    setRemoteNickname(from.nickname);
+  socket.on('connection:accepted', (sender) => {
+    setRemotePeerId(sender.peerId);
+    setRemoteNickname(sender.nickname);
     setRemotePeerConnected(true);
+    setReceiveStatus('connected');
+    setReceiveMessage(`Connected to peer - ${sender.peerId}`);
 
-    setReceiveStatus('connected', `Connected to peer - ${from.peerId}`);
+    initPeerConnection();
   });
 }
