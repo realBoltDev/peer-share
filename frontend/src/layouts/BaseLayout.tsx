4547ba65b9dcb7dafcdea119a26a5533a4e4b98a
beastxbolt <harsit2405@gmail.com>
2025-11-19 13:16:48 +0530

    feat: Add useEffect for socket init

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from 'react';
 import { AppShell } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Outlet } from 'react-router-dom';
 import { Header } from '@/components/Header/Header';
 import { Sidebar } from '@/components/Sidebar/Sidebar';
+import { useAppStore } from '@/store/appStore';
 
 export function BaseLayout() {
   const [opened, { toggle }] = useDisclosure();
+  const initSocketConn = useAppStore((s) => s.initSocketConn);
+
+  useEffect(() => {
+    initSocketConn();
+  }, []);
 
   return (
     <AppShell
