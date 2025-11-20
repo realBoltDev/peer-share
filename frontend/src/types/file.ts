d7cb53fa0bae832593ca743780c123b0ef755a26
beastxbolt <harsit2405@gmail.com>
2025-11-20 11:53:09 +0530

    chore: Update file types

--- frontend/src/types/file.ts
+++ frontend/src/types/file.ts
@@ -3,13 +3,3 @@ import { FileWithPath } from '@mantine/dropzone';
 export interface FileUploadProps {
   onFilesAdd: (files: FileWithPath[]) => void;
 }
-
-interface FileMetadata {
-  filename: string
-  size: string
-  speed: string
-  progress: number
-  status: string
-}
-
-export type FileProps = FileMetadata[];
