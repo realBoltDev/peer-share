48e29f9df8e1b5bcb3d882171750472d17671b45
beastxbolt <harsit2405@gmail.com>
2025-11-20 23:44:29 +0530

    feat: Add website logo svg

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -7,10 +7,12 @@ export function Header({ opened, toggle }: HeaderProps) {
     <>
       <header className={classes.header}>
         <Group h="100%" justify="space-between" align="center">
-          <Group gap="xs" align="center">
+          <Group gap="8px" align="center">
             <div style={{ display: 'flex', alignItems: 'center' }}>
-              <svg width="32" height="32" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ flexShrink: 0 }}>
-                <path fillRule="evenodd" clipRule="evenodd" d="M7.289.797a.5.5 0 0 1 .422 0l6 2.8A.5.5 0 0 1 14 4.05v6.9a.5.5 0 0 1-.289.453l-6 2.8a.5.5 0 0 1-.422 0l-6-2.8A.5.5 0 0 1 1 10.95v-6.9a.5.5 0 0 1 .289-.453zM2 4.806 7 6.93v6.034l-5-2.333zm6 8.159 5-2.333V4.806L8 6.93zm-.5-6.908 4.772-2.028L7.5 1.802 2.728 4.029z" fill="#C9C9C9" />
+              <svg width="40" height="40" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                <path stroke="#fff" strokeWidth="12" strokeLinecap="round" d="m60 140 80-80" />
+                <circle cx="60" cy="140" r="28" fill="none" stroke="#fff" strokeWidth="12" />
+                <circle cx="140" cy="60" r="34" fill="#fff" />
               </svg>
             </div>
 
