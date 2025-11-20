3028567e736bb76d14ad51ca1d83ebeb79550788
beastxbolt <harsit2405@gmail.com>
2025-11-20 23:49:44 +0530

    refactor: Add redis connect call

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
+import { config } from './config.js';
 import http from 'http';
 import { Server } from 'socket.io';
 import { initSockets } from './sockets/socket.js';
 import { app } from './app.js';
+import { connectRedis } from './redis/client.js';
 
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
@@ -9,9 +11,11 @@ const io = new Server(server, { cors: { origin: '*' } });
 initSockets(io);
 
 app.get('/', (req, res) => {
-    res.status(200).json({ success: true, message: 'PeerShare Server active'});
+  res.status(200).json({ success: true, message: 'PeerShare Server active' });
 });
 
-server.listen(3000, () => {
-    console.log('[INFO] Server running on port 3000');
+await connectRedis();
+
+server.listen(config.express.port, () => {
+  console.log(`[INFO] Server running on port ${config.express.port}`);
 })
